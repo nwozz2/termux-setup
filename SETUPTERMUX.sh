0f147f88e4cc657aadcbebf8901becc1dfec2ebb
nwozz2 <64344770+nwozz2@users.noreply.github.com>
2020-12-15 02:49:55 +0530

    Add files via upload

---
+++ SETUPTERMUX.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "THANK YOU ! SETTING TERMUX"
+sleep 2
+echo "MADE BY NWOz CHOCO @WORLD ORDER OWNER"
+sleep 3
+echo "Script will started in 5 seconds"
+sleep 5 
+
+
+apt install git
+
+apt install git
+pkg install python2
+pkg install python
+pkg install figlet
+gem install bundler
+pkg install php
+pkg install openssh
+pkg install openssh-tool
+pkg install curl
+pkg install ruby
+pkg install mc
+pip install --upgrade pip
+apt update 
+apt upgrade
+pkg install unstable-repo
+pkg install root-repo
+pkg install metasploit
+pkg install toilet
+gem install pip
+pkg install nano
+
+
+echo "COMPLETED ! ! ! "
+sleep 4
+echo "THANK YOU FOR USING NWOz TERMUX SETUP TOOL"
+sleep 4 
+echo "script exiting.."
+sleep 1
